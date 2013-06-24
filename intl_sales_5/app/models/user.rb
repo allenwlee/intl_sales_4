21d891977e92c83b0a366f7731d6356cacd94349
Allen Lee <allenwlee@Allens-MacBook-Pro.local>
2013-06-24 09:47:21 -0700

    bcrypt install

--- intl_sales_5/app/models/user.rb
+++ intl_sales_5/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
   attr_accessible :username, :password, :email, :category
   has_many :projects
+  has_secure_password
+  validates :username, :email, :presence => true
+
 end
